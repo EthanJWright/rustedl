ac81a583a949b1167f8be9c06696cce9527a0718
EthanJWright <ewright362@gmail.com>
2022-06-24 01:20:44 -0600

    hooking up front end to back end

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,4 +1,17 @@
 use yew::prelude::*;
+use wasm_bindgen::JsValue;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
+use web_sys::window;
+
+
+
+#[wasm_bindgen(module = "/public/glue.js")]
+extern "C" {
+    #[wasm_bindgen(js_name = invokeHello, catch)]
+    pub async fn hello(name: String) -> Result<JsValue, JsValue>;
+}
+
 
 fn main() {
     yew::start_app::<App>();
@@ -6,10 +19,47 @@ fn main() {
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let welcome = use_state_eq(|| "".to_string());
+    let name = use_state_eq(|| "Ethan".to_string());
+
+    // Execute tauri command via effects.
+    // The effect will run every time `name` changes.
+    {
+        let welcome = welcome.clone();
+        use_effect_with_deps(
+            move |name| {
+                update_welcome_message(welcome, name.clone());
+                || ()
+            },
+            (*name).clone(),
+        );
+    }
+
+    let message = (*welcome).clone();
+
     html! {
         <div>
-            <h2 class={"heading"}>{"Hello, World!"}</h2>
+            <h2 class={"heading"}>{message}</h2>
         </div>
     }
+}
+
+fn update_welcome_message(welcome: UseStateHandle<String>, name: String) {
+    spawn_local(async move {
+        // This will call our glue code all the way through to the tauri
+        // back-end command and return the `Result<String, String>` as
+        // `Result<JsValue, JsValue>`.
+        match hello(name).await {
+            Ok(message) => {
+                welcome.set(message.as_string().unwrap());
+            }
+            Err(e) => {
+                let window = window().unwrap();
+                window
+                    .alert_with_message(&format!("Error: {:?}", e))
+                    .unwrap();
+            }
+        }
+    });
 }
 
--- frontend/target/wasm32-unknown-unknown/debug/frontend.d
+++ frontend/target/wasm32-unknown-unknown/debug/frontend.d
@@ -1,1 +1,1 @@
-/home/ethan/codespace/rustedl/frontend/target/wasm32-unknown-unknown/debug/frontend.wasm: /home/ethan/codespace/rustedl/frontend/src/main.rs
+/home/ethan/codespace/rustedl/frontend/target/wasm32-unknown-unknown/debug/frontend.wasm: /home/ethan/codespace/rustedl/frontend/public/glue.js /home/ethan/codespace/rustedl/frontend/src/main.rs
